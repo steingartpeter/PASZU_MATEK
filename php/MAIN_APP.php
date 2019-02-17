6cc4451f3bac99ba029072054b85bad43bf4b2c9
steingartpeter <steingart.p2@gmail.com>
2019-02-17 17:00:17 +0100

    2019-02-17 001 AX07057  Basic setup 01 HOME Linux X540S

---
+++ php/MAIN_APP.php
@@ -0,0 +1,48 @@
+<?
+<M></M>
+//×-
+//@-MODULÉV   : PASZU_MATEK - MAIN_APP.php-@
+//@-SZERZŐ    : PETER STEINGART-@
+//@-LÉTREHOZVA: 2019-02-17-@
+//@-FÜGGŐSÉGEK:
+//×-
+// @-- NO DEPENDENCIES - BASIC FILE-@
+//-@
+//-×
+//-@
+//@-LEÍRÁS    :<br/>
+// Basis of th eapplication, mostly with HTML generation, and sub class using functions.<br/>
+//@-MÓDOSÍTÁSOK :
+//×-
+// @-- YYYY-MM-DD
+// -@
+// @-- ... -@
+//-×
+//-×
+//</M>
+
+class MAIN_APP{
+    //<nn>
+    // The basic class of the application, manages the cooperation of the elements.
+    //</nn>
+
+    public function __construct(){
+        //<SF>
+        //LÉTREHOZVA: 2019-02-17<br/>
+        //SZERZÓ: PETER STEINGART<br/>
+        //LEÍRÁS: The normal basic contructor.<br/>
+        //</SF>
+
+        echo '<p>MAIN APP CONSTRUCTION WAS DONE....</p>';
+
+
+    }
+    
+    
+
+    
+}
+
+
+
+?>
